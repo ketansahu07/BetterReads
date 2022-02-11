9edef9141d81c182cc4ca2e99e04264c5fe6b37c
ketansahu07 <ketankumar120@gmail.com>
2022-02-11 23:53:19 +0530

    authors data saved in db

--- betterreads-data-loader/src/main/java/com/betterreads/betterreadsdataloader/BetterreadsDataLoaderApplication.java
+++ betterreads-data-loader/src/main/java/com/betterreads/betterreadsdataloader/BetterreadsDataLoaderApplication.java
@@ -1,25 +1,81 @@
 package com.betterreads.betterreadsdataloader;
 
+import com.betterreads.betterreadsdataloader.author.Author;
+import com.betterreads.betterreadsdataloader.author.AuthorRepository;
 import connection.DataStaxAstraProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
+@Slf4j
 @SpringBootApplication
 @EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class BetterreadsDataLoaderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BetterreadsDataLoaderApplication.class, args);
+	@Autowired
+	AuthorRepository authorRepository;
+
+	@Value("${datadump.location.authors}")
+	private String authorDumpLocation;
+
+	@Value("${datadump.location.works}")
+	private String worksDumpLocation;
+
+	private void initAuthors() {
+		Path path = Paths.get(authorDumpLocation);
+		try (Stream<String> lines = Files.lines(path)) {
+			lines.forEach(line -> {
+				String jsonString = line.substring(line.indexOf("{"));
+				try {
+					JSONObject jsonObject = new JSONObject(jsonString);
+
+					Author author = new Author();
+					author.setName(jsonObject.optString("name"));
+					author.setPersonalName(jsonObject.optString("personal_name"));
+					author.setId(jsonObject.optString("key").replace("/authors/", ""));
+
+					log.info("Saving {} to db.", author);
+					authorRepository.save(author);
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void initWorks() {
+
+	}
+
+	@PostConstruct
+	public void start() {
+		initAuthors();
+		initWorks();
 	}
 
 	@Bean
 	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
 		Path bundle = astraProperties.getSecureConnectBundle().toPath();
 		return builder -> builder.withCloudSecureConnectBundle(bundle);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterreadsDataLoaderApplication.class, args);
 	}
 }
